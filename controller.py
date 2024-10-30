b69b8d5b603c4715b93d0cb1c754b639aeead04e
leprekus <leprekus@yahoo.com>
2024-10-30 13:57:22 -0700

    start controller

---
+++ controller.py
@@ -0,0 +1,12 @@
+from chessBoard import *
+
+class Controller:
+    __init__():
+        this.chessBoard = chessBoard()
+        # TODO implement and import player class
+        this.white = None
+        this.black = None
+        this.turnCount = 0
+
+    def 
+
