e1e8a9ec0749311f3a4caa78c99bb00e6f820366
evanotoole123 <evankarlotoole@gmail.com>
2024-12-22 13:53:12 -0800

    made methods to see if there is checkmate, check, and validates moves etc. these methods are still untested howver

--- referee.py
+++ referee.py
@@ -6,7 +6,7 @@ from knight import Knight
 from queen import Queen
 from bishop import Bishop
 from king import King
-from typing import List
+from typing import Any, List
 
 
 class Referee:
@@ -14,16 +14,142 @@ class Referee:
         self.chessboard_dict = ChessBoard().chessBoard
         self.white_check = white_check
         self.taken_by_white = []
-
         self.black_check = black_check
         self.taken_by_black = []
 
+
+    def revert(self, prev_pos: str, curr_pos:str, taken_piece:object):
+        self.chessboard_dict[prev_pos] = self.chessboard_dict[curr_pos]
+        self.chessboard_dict[curr_pos] = taken_piece
+
+
+    def checkmate(self)->bool:
+        white_check = self.white_in_check('white')
+        black_check = self.black_in_check('black')
+        all_black_reached = self.get_all_reachable_black()
+        all_white_reached = self.get_all_reachable_white()
+        white_king_square = self.get_white_king_square()
+        black_king_square = self.get_black_king_square()
+        white_king = self.chessboard_dict[white_king_square]
+        black_king = self.chessboard_dict[black_king_square]
+        #if we are currently in check
+        if white_check:
+            #get possible squares the white king can move to
+            white_movable_squares = self.find_reachable_squares(white_king)
+            for square in white_movable_squares:
+                #if the king can move to a square that is not reachable by black it is not checkmate
+                if square not in all_black_reached:
+                    return False
+            for piece in self.chessboard_dict.values():
+                if piece.color == 'white':
+                    if self.is_pinnable(piece):
+                        return False
+            return True
+        
+        if black_check:
+            black_movable_squares = self.find_reachable_squares(black_king)
+            for square in black_movable_squares:
+                if square not in all_white_reached:
+                    return False
+            for piece in self.chessboard_dict.values():
+                if piece.color == 'black':
+                    if self.is_pinnable(piece):
+                        return False
+            return True
+        
+        return False
+
+
+    def is_pinnable(self, piece: object)->bool:
+
+        if piece.color == 'white':
+            reachable = self.find_reachable_squares(piece)
+            for square in reachable:
+                move = self.move(piece.current_pos, square)
+                if not self.white_in_check(piece.color):
+                    self.revert(piece.current_pos, square, self.chessboard_dict[square])
+                    return True
+                elif move:
+                    self.revert(piece.current_pos, square, self.chessboard_dict[square])
+            return False
+                 
+        else:
+            reachable = self.find_reachable_squares(piece)
+            for square in reachable:
+                move = self.move(piece.current_pos, square)
+                if not self.black_in_check(piece.color):
+                    self.revert(piece.current_pos, square, self.chessboard_dict[square])
+                    return True
+                elif move:
+                    self.revert(piece.current_pos, square, self.chessboard_dict[square])
+            return False
+           
     
-    def validate_move(self, move: List[str])->bool:
+    def validate_move(self, curr_piece: object, taken_piece:object, moves: List[str])->bool:
+        current_pos = curr_piece.current_pos
+        new_pos = taken_piece.current_pos
+        #checks if the square is in the possible moves for the piece
+        #then checks if the move put ourselves in check (if it does we can't do the move)
+
+        if new_pos in moves:
+            self.chessboard_dct[new_pos] = curr_piece
+            self.chessboard_dict[current_pos] = None
+            if curr_piece.color == 'white':
+                self.white_check = self.white_in_check(curr_piece.color)
+                if self.white_check:
+                    self.revert(current_pos, new_pos, taken_piece)
+                    return False
+            else:
+                self.black_check = self.black_in_check(curr_piece.color)
+                if self.black_check:
+                    self.revert(current_pos, new_pos, taken_piece)
+                    return False
+                
+        self.revert(current_pos, new_pos, taken_piece)
+        return True
         pass
     
-    def is_in_check(self, color: str)->bool:
-        pass
+    
+
+    def white_in_check(self, color: str)->bool:
+        white_king = self.get_white_king_square()
+        black_reachable = self.get_all_reachable_black()
+        if white_king in black_reachable:
+            return True
+        return False
+    
+        
+    def black_in_check(self, color: str)->bool:
+        black_king = self.get_black_king_square()
+        white_reachable = self.get_all_reachable_white()
+        if black_king in white_reachable:
+            return True
+        return False
+    
+
+    def get_white_king_square(self)->str:
+        for object in self.chessboard_dict.values():
+            if type(object) == King and object.color == 'white':
+                return object.current_pos
+        
+    def get_black_king_square(self)->str:
+        for object in self.chessboard_dict.values():
+            if type(object) == King and object.color == 'black':
+                return object.current_pos
+            
+    def get_all_reachable_white(self)->List[str]:
+        reachable_squares = []
+        for object in self.chessboard_dict.values():
+            if object.color == 'white':
+                reachable_squares.extend(self.find_reachable_squares(object))
+        return reachable_squares
+    
+    def get_all_reachable_black(self)->List[str]:
+        reachable_squares = []
+        for object in self.chessboard_dict.values():
+            if object.color == 'black':
+                reachable_squares.extend(self.find_reachable_squares(object))
+        return reachable_squares
 
     #when a piece takes another just replace one piece with the other, and change
     def take_square(self, piece: Piece, square: str):
@@ -57,11 +183,15 @@ class Referee:
         
         return reachable_line
 
-    def move(self, curr_pos: str, new_pos: str):
+    def move(self, curr_pos: str, new_pos: str) -> bool:
 
             curr_piece = self.chessboard_dict[ curr_pos ]
             taken_piece = self.chessboard_dict[ new_pos ]
             valid_moves = self.find_reachable_squares(curr_piece)
+
+            valid_move = self.validate_move(curr_piece, new_pos, taken_piece, valid_moves)
+            if not valid_move:
+                return False
 
             # remove opponent piece and add it to list of taken pieces
             if isinstance(taken_piece, Piece): 
@@ -71,13 +201,11 @@ class Referee:
                     self.taken_by_black.append(taken_piece)
 
             # update gameboard with moved piece
-            if new_pos in valid_moves:
-                self.chessboard_dict[ curr_piece.current_pos ] = None
-                self.chessboard_dict[ new_pos ] = curr_piece 
-                curr_piece.current_pos = new_pos
+            self.chessboard_dict[ curr_piece.current_pos ] = None
+            self.chessboard_dict[ new_pos ] = curr_piece 
+            curr_piece.current_pos = new_pos
 
-            #will remove this, just want to make sure pieces are moved correctly
-            return curr_piece
+            return True
 
     
     
