085afa0b7933283349bdc8e538291e73b42f88f6
leprekus <leprekus@yahoo.com>
2024-12-22 19:00:51 -0600

    implement classes for turn based handling

--- server/match_making.py
+++ server/match_making.py
@@ -1,19 +1,41 @@
 
-from typing import Dict
+from typing import Dict, Optional, TypedDict
 from referee import Referee
 from server.illegal_match_access_error import IllegalMatchAccessException
 
-MatchesSchema = Dict[ str, Referee ]
+from uuid import uuid4
+
+class Player:
+    def __init__(self, id: str):
+        self.id = id
+
+class Match:
+    def __init__(self, session_id: str, p1: str, p2: str):
+        self.referee = Referee()
+        self.session_id = session_id
+        self.p1 = Player(p1)
+        self.p2 = Player(p2)
+
+MatchesSchema = Dict[ str, Match ]
 class MatchMaking:
     def __init__(self):
         self.matches: MatchesSchema = {}
 
-    def create_game(self, sesh_id: str):
-        self.matches[ sesh_id ] = Referee()
-        return self.matches[ sesh_id ]
+    def create_game(self) -> tuple[ str, str ]:
+        session_id, p1, p2 = str(uuid4()), str(uuid4()), str(uuid4())
+        M = Match( session_id, p1, p2 )
+        self.matches[ M.session_id ] =  M
+        return  ( M.session_id, M.p1.id )
 
-    def get_game(self, sesh_id: str):
+    def join_game(self, sesh_id: str) -> str:
+        if not self.matches[ sesh_id ]:
+            raise IllegalMatchAccessException('Trying to access a non-existent match') 
+        return self.matches[ sesh_id ].p2.id
+
+    def get_game(self, sesh_id: str) -> Match:
         if not self.matches[ sesh_id ]:
             raise IllegalMatchAccessException('Trying to access a non-existent match') 
         return self.matches[ sesh_id ]
+
+
 
